5ec890f5254e42f27b02072d92c6fcd0172fb440
VccLabs <thevcclabs@gmail.com>
2026-06-18 11:47:26 +0300

    Updating Giraffe IDE files

--- GiraffeIDE/PD_V0.5/libs/appscc/grf_prj_cc.h
+++
@@ -1,4 +0,0 @@
-static grf_view_fun_t grf_views_fun[] = {
-	{GRF_VIEW1_ID,view1_init,view1_entry,view1_exit }
-};
-
--- GiraffeIDE/PD_V0.5/libs/appscc/view1_cc.h
+++
@@ -1,5 +0,0 @@
-static grf_ctrl_fun_t view_ctrls_fun_t[] =
-{
-	{VIEW1_LABEL0_ID,label0_event},
-	{VIEW1_BUTTON0_ID,button0_event}
-};
